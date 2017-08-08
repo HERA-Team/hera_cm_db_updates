4a01a6306a3eb3ecce1e863c5f4512ef7155ab5c
David DeBoer <ddeboer@berkeley.edu>
2017-08-08 10:50:21 -0700

    added antennas connected as reported by Deepthi

---
+++ scripts/open_new_C7_RI_170808.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+script_file = open('open_new_C7_RI_170808','w')
+
+data_arr = [
+['C7F84','A','eb','RI8A1E','A','a'],
+['C7F84','A','nb','RI8A1N','A','a'],
+['C7F85','A','eb','RI8A2E','A','a'],
+['C7F85','A','nb','RI8A2N','A','a'],
+['C7F86','A','eb','RI8A3E','A','a'],
+['C7F86','A','nb','RI8A3N','A','a'],
+['C7F87','A','eb','RI8A4E','A','a'],
+['C7F87','A','nb','RI8A4N','A','a'],
+['C7F52','A','eb','RI8A5E','A','a'],
+['C7F52','A','nb','RI8A5N','A','a'],
+['C7F53','A','eb','RI8A6E','A','a'],
+['C7F53','A','nb','RI8A6N','A','a'],
+['C7F54','A','eb','RI8A7E','A','a'],
+['C7F54','A','nb','RI8A7N','A','a'],
+['C7F55','A','eb','RI8A8E','A','a'],
+['C7F55','A','nb','RI8A8N','A','a'],
+['C7F24','A','eb','RI8B1E','A','a'],
+['C7F24','A','nb','RI8B1N','A','a'],
+['C7F25','A','eb','RI8B2E','A','a'],
+['C7F25','A','nb','RI8B2N','A','a'],
+['C7F26','A','eb','RI8B3E','A','a'],
+['C7F26','A','nb','RI8B3N','A','a'],
+['C7F27','A','eb','RI8B4E','A','a'],
+['C7F27','A','nb','RI8B4N','A','a'],
+['C7F36','A','eb','RI6A1E','A','a'],
+['C7F36','A','nb','RI6A1N','A','a'],
+['C7F51','A','eb','RI6A2E','A','a'],
+['C7F51','A','nb','RI6A2N','A','a'],
+['C7F69','A','eb','RI6A3E','A','a'],
+['C7F69','A','nb','RI6A3N','A','a'],
+['C7F70','A','eb','RI6A4E','A','a'],
+['C7F70','A','nb','RI6A4N','A','a']
+]
+
+use_date = '2017/08/01'
+do_it_this_time = True
+
+for data in data_arr:
+    s = 'add_connection.py --date {} -u {} -d {} '.format(use_date,data[0],data[3])
+    s+= '--uprev {} --upport {} '.format(data[1],data[2])
+    s+= '--dnrev {} --dnport {} '.format(data[4],data[5])
+    if do_it_this_time:
+        s+= '--actually_do_it\n'
+    else:
+        s+='\n'
+    script_file.write(s)
