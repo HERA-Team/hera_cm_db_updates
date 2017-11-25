d273918300ff599428650b05eef97b50636de173
David DeBoer <ddeboer@berkeley.edu>
2017-11-25 10:30:38 -0800

    added new ants

--- scripts/171125_script.py
+++ scripts/171125_script.py
@@ -1,26 +1,13 @@
 #! /usr/bin/env python
 from __future__ import absolute_import, division, print_function
 import def_pc as pc
+import sys
 
 log_file = 'scripts.log'
 do_it_this_time = True
 
-connectA142_to_stop = [
-                      [['C7F142', 'A', 'eb'], ['RI4B7E', 'A', 'a'], '2017/11/19'],
-                      [['C7F142', 'A', 'nb'], ['RI4B7N', 'A', 'a'], '2017/11/19'],
-                      [['FEA32', 'A', 'e'], ['C7F142', 'A', 'ea'], '2017/11/19'],
-                      [['FEA32', 'A', 'n'], ['C7F142', 'A', 'na'], '2017/11/19'],
-                      [['FDA32', 'B', 'terminals'], ['FEA32', 'A', 'input'], '2017/11/19']
-]
-connectA142_to_strt = [
-                      [['FDA32', 'B', 'terminals'], ['FEM75041', 'A', 'input'], '2017/11/23'],
-                      [['FEM75041', 'A', 'e'], ['C7F142', 'A', 'ea'], '2017/11/23'],
-                      [['FEM75041', 'A', 'n'], ['C7F142', 'A', 'na'], '2017/11/23'],
-                      [['C7F142', 'A', 'eb'], ['RI3A6E', 'A', 'a'], '2017/11/23'],
-                      [['C7F142', 'A', 'nb'], ['RI3A6N', 'A', 'a'], '2017/11/23']
-]
-fems_to_add = ['75041', '75042', '75043', '75044', '75045', '75046']
-pams_to_add = ['75117', '75118', '75119', '75120', '75121', '75122']
+fems_to_add = ['75042', '75043', '75044', '75045', '75046']
+pams_to_add = ['75117', '75118', '75119', '75120', '75121']
 ants_to_add = [
               ['HH136', '1', 'S/N53', 'FEM75046', 'RI3A1', 'PAM75117', '2017/11/24'],
               ['HH137', '2', 'S/N54', 'FEM75045', 'RI3A2', 'PAM75118', '2017/11/24'],
@@ -29,7 +16,7 @@ ants_to_add = [
               ['HH140', '30', 'S/N57', 'FEM75042', 'RI3A5', 'PAM75121', '2017/11/24']
 ]
 
-fp = pc.init_script()
+fp = pc.init_script(sys.argv, log_file)
 # Add FEM/PAM to parts
 cdate = '2017/11/23'
 ctime = '10:00'
@@ -48,16 +35,13 @@ for p in pams_to_add:
     part = [hpn, rev, htype, serno]
     fp.write(pc.part(part, cdate, ctime, do_it_this_time))
 
-for c in connections_to_stop:
-    fp.write(pc.connect(['stop']))
-
 for a in ants_to_add:
     new_station = a[0]
     hhno = a[0][2:]
     new_ant = 'A' + hhno
     new_feed = 'FDA' + a[1]
     feed_mfg = 'P' + a[1]
-    nrao_frend = 'FEA' + a[1]
+    nrao_frend = 'FEA' + a[1]  # Keeping this just for information
     new_cable = 'C7F' + hhno
     serno = a[2]
     new_fem = a[3]
@@ -69,32 +53,27 @@ for a in ants_to_add:
 
     val_na = int(hhno)
 
-    if new_fem[:3] == 'FEM':
-        new_front_end = new_fem
-    else:
-        new_front_end = nrao_frend
     if new_pam[:3] == 'PAM':
         for p in ['E', 'N']:
             ri = new_rator_in + p
             pin = p.lower() + 'a'
-            fp.write(connect('add', [ri, 'A', 'b'], [new_pam, 'B', pin], cdate, ctime, do_it_this_time))
+            fp.write(pc.connect('add', [ri, 'A', 'b'], [new_pam, 'B', pin], cdate, ctime, do_it_this_time))
             ro = new_rator_out + p
             pout = p.lower() + 'b'
-            fp.write(connect('add', [new_pam, 'B', pout], [ro, 'A', 'b'], cdate, ctime, do_it_this_time))
+            fp.write(pc.connect('add', [new_pam, 'B', pout], [ro, 'A', 'b'], cdate, ctime, do_it_this_time))
 
     s = 'add_station.py {}\n'.format(new_station)
     fp.write(s)
-    fp.write(part([new_ant, 'H', 'antenna', serno], cdate, ctime, do_it_this_time))
-    fp.write(part([new_feed, 'B', 'feed', feed_mfg], cdate, ctime, do_it_this_time))
+    fp.write(pc.part([new_ant, 'H', 'antenna', serno], cdate, ctime, do_it_this_time))
+    fp.write(pc.part([new_feed, 'B', 'feed', feed_mfg], cdate, ctime, do_it_this_time))
     if val_na > 127:
-        fp.write(part([new_cable, 'A', 'cable-feed75', new_cable], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_station, 'A', 'ground'], [new_ant, 'H', 'ground'], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_ant, 'H', 'focus'], [new_feed, 'B', 'input'], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_feed, 'B', 'terminals'], [new_front_end, 'A', 'input'], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_front_end, 'A', 'e'], [new_cable, 'A', 'ea'], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_front_end, 'A', 'n'], [new_cable, 'A', 'na'], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_cable, 'A', 'eb'], [new_rator + 'E', 'A', 'a'], cdate, ctime, do_it_this_time))
-    fp.write(connect('add', [new_cable, 'A', 'nb'], [new_rator + 'N', 'A', 'a'], cdate, ctime, do_it_this_time))
-
+        fp.write(pc.part([new_cable, 'A', 'cable-feed75', new_cable], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_station, 'A', 'ground'], [new_ant, 'H', 'ground'], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_ant, 'H', 'focus'], [new_feed, 'B', 'input'], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_feed, 'B', 'terminals'], [new_fem, 'A', 'input'], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_fem, 'A', 'e'], [new_cable, 'A', 'ea'], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_fem, 'A', 'n'], [new_cable, 'A', 'na'], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_cable, 'A', 'eb'], [new_rator_in + 'E', 'A', 'a'], cdate, ctime, do_it_this_time))
+    fp.write(pc.connect('add', [new_cable, 'A', 'nb'], [new_rator_in + 'N', 'A', 'a'], cdate, ctime, do_it_this_time))
 
 fp.close()
