5882b276f469fc9763d294a60b74b8d07c08ca96
Paul La Plante <plaplant@sas.upenn.edu>
2017-12-08 16:15:05 -0500

    Update current list of dubitables; backfill for start of H1C season

--- scripts/dubi.py
+++ scripts/dubi.py
@@ -11,9 +11,9 @@ from __future__ import absolute_import, division, print_function
 from hera_mc import part_connect
 from astropy.time import Time
 
-ant_list = ['4', '5', '6', '7', '8', '9',
+ant_list = ['3', '4', '5', '6', '7', '8', '9',
             '10', '15', '16', '17', '18', '19',
-            '20', '21', '22', '29',
+            '20', '21', '22', '28', '29',
             '30', '31', '32', '33', '34', '35',
             '42', '43', '44', '45', '46', '47', '48', '49',
             '50', '56', '57', '58', '59',
