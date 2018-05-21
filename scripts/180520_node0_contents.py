48ff2beafe43ba85e3cbf6cdbb544222cb7f2139
David DeBoer <ddeboer@berkeley.edu>
2018-05-21 14:15:32 -0700

    added signal paths 0, 1, 12

--- scripts/180520_node0_contents.py
+++ scripts/180520_node0_contents.py
@@ -10,26 +10,26 @@ do_it_this_time = True
 
 node = 'ND0'
 
-parts_to_add = {'N0': ['A', 'node', 'N0', 'ground>ND0:A:ground'],
-                'PCH1': ['A', 'pam-chassis', 'PCH1', 'rack>N0:A:loc0'],
-                'PAM01': ['A', 'pam', 'PAM1', 'slot>PCH1:A:slot01'],
-                'PAM02': ['A', 'pam', 'PAM2', 'slot>PCH1:A:slot02'],
-                'PAM03': ['A', 'pam', 'PAM3', 'slot>PCH1:A:slot03'],
-                'PAM04': ['A', 'pam', 'PAM4', 'slot>PCH1:A:slot04'],
-                'PAM05': ['A', 'pam', 'PAM5', 'slot>PCH1:A:slot05'],
-                'PAM06': ['A', 'pam', 'PAM6', 'slot>PCH1:A:slot06'],
-                'PAM07': ['A', 'pam', 'PAM7', 'slot>PCH1:A:slot07'],
-                'PAM08': ['A', 'pam', 'PAM8', 'slot>PCH1:A:slot08'],
-                'PAM09': ['A', 'pam', 'PAM9', 'slot>PCH1:A:slot09'],
-                'PAM10': ['A', 'pam', 'PAM10', 'slot>PCH1:A:slot10'],
-                'PAM11': ['A', 'pam', 'PAM11', 'slot>PCH1:A:slot11'],
-                'PAM12': ['A', 'pam', 'PAM12', 'slot>PCH1:A:slot12'],
+parts_to_add = {'N0': ['A', 'node', 'N0', '@ground>ND0:A:@ground'],
+                'PCH1': ['A', 'pam-chassis', 'PCH1', '@rack>N0:A:l@oc0'],
+                'PAM01': ['A', 'pam', 'PAM1', '@slot>PCH1:A:@slot01'],
+                'PAM02': ['A', 'pam', 'PAM2', '@slot>PCH1:A:@slot02'],
+                'PAM03': ['A', 'pam', 'PAM3', '@slot>PCH1:A:@slot03'],
+                'PAM04': ['A', 'pam', 'PAM4', '@slot>PCH1:A:@slot04'],
+                'PAM05': ['A', 'pam', 'PAM5', '@slot>PCH1:A:@slot05'],
+                'PAM06': ['A', 'pam', 'PAM6', '@slot>PCH1:A:@slot06'],
+                'PAM07': ['A', 'pam', 'PAM7', '@slot>PCH1:A:@slot07'],
+                'PAM08': ['A', 'pam', 'PAM8', '@slot>PCH1:A:@slot08'],
+                'PAM09': ['A', 'pam', 'PAM9', '@slot>PCH1:A:@slot09'],
+                'PAM10': ['A', 'pam', 'PAM10', '@slot>PCH1:A:@slot10'],
+                'PAM11': ['A', 'pam', 'PAM11', '@slot>PCH1:A:@slot11'],
+                'PAM12': ['A', 'pam', 'PAM12', '@slot>PCH1:A:@slot12'],
                 'SNP1': ['A', 'snap', 'SNP1', 'rack>N0:A:loc1'],
                 'SNP2': ['A', 'snap', 'SNP2', 'rack>N0:A:loc2'],
                 'SNP3': ['A', 'snap', 'SNP3', 'rack>N0:A:loc3'],
                 'SNP4': ['A', 'snap', 'SNP4', 'rack>N0:A:loc4'],
-                'PCM1': ['A', 'power-control-module', 'PCM1', 'rack>N0:A:loc5'],
-                'FPS1': ['A', 'fem-power-supply', 'FPS1', 'rack>N0:A:loc6']}
+                'PCM1': ['A', 'power-control-module', 'PCM1', '@rack>N0:A:@loc5'],
+                'FPS1': ['A', 'fem-power-supply', 'FPS1', '@rack>N0:A:@loc6']}
 
 fp = pc.init_script(sys.argv, log_file)
 
@@ -57,10 +57,10 @@ ctime = '12:00'
 for p, d in parts_to_add.iteritems():
     upart = p
     urev = d[0]
-    uport = '@' + d[3].split('>')[0]
+    uport = d[3].split('>')[0]
     dpart = d[3].split('>')[1].split(':')[0]
     drev = d[3].split('>')[1].split(':')[1]
-    dport = '@' + d[3].split('>')[1].split(':')[2]
+    dport = d[3].split('>')[1].split(':')[2]
     add_connection(upart, urev, uport, dpart, drev, dport, cdate, ctime, do_it_this_time)
 
 fp.close()
---
+++ scripts/180521_add_sigpath_A1.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+from __future__ import absolute_import, division, print_function
+import def_pc as pc
+import sys
+
+print("Set up new sigpath")
+
+log_file = 'scripts.log'
+do_it_this_time = True
+
+parts_to_add = [['FDV2', 'A', 'feed', 'MIT2'],
+                ['FEM04', 'A', 'fem', 'FEMxxx04'],
+                ['CRF001', 'A', 'cable-rfof', 'browngreen']]
+connections_to_add = [[['A1', 'H', 'focus'], ['FDV2', 'A', 'input']],
+                      [['FDV2', 'A', 'terminals'], ['FEM04', 'A', 'input']],
+                      [['FEM04', 'A', 'e'], ['CRF001', 'A', 'ea']],
+                      [['FEM04', 'A', 'n'], ['CRF001', 'A', 'na']],
+                      [['CRF001', 'A', 'eb'], ['PAM02', 'A', 'ea']],
+                      [['CRF001', 'A', 'nb'], ['PAM02', 'A', 'na']],
+                      [['PAM02', 'A', 'eb'], ['SNP1', 'A', 'e2']],
+                      [['PAM02', 'A', 'nb'], ['SNP1', 'A', 'n2']]]
+
+fp = pc.init_script(sys.argv, log_file)
+
+
+def add_part(hpn, rev, htype, serno, cdate, ctime, do_it_this_time):
+    part = [hpn, rev, htype, serno]
+    fp.write(pc.part('add', part, cdate, ctime, do_it_this_time))
+
+
+def add_connection(upart, urev, uport, dpart, drev, dport, cdate, ctime, do_it_this_time):
+    up = [upart, urev, uport]
+    down = [dpart, drev, dport]
+    fp.write(pc.connect('add', up, down, cdate, ctime, do_it_this_time))
+
+
+cdate = '2018/05/21'
+
+ctime = '11:00'
+for p in parts_to_add:
+    add_part(p[0], p[1], p[2], p[3], cdate, ctime, do_it_this_time)
+
+ctime = '12:00'
+for c in connections_to_add:
+    upart = c[0][0]
+    urev = c[0][1]
+    uport = c[0][2]
+    dpart = c[1][0]
+    drev = c[1][1]
+    dport = c[1][2]
+    add_connection(upart, urev, uport, dpart, drev, dport, cdate, ctime, do_it_this_time)
+
+fp.close()
