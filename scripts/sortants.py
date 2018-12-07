57811a7586afb1708fcd9427877b59618efddfd1
ddeboer <ddeboer@berkeley.edu>
2018-12-07 09:07:22 -0800

    made sortants a script

--- scripts/sortants.py
+++ scripts/sortants.py
@@ -1,25 +1,35 @@
-def doit(fn):
-    ants = {}
-    with open(fn, 'r') as fp:
-        for line in fp:
-            data = line.split()
-            ano = int(data[0][2:])
-            ana = data[0]
-            E = float(data[1])
-            N = float(data[2])
-            lng = float(data[3])
-            lat = float(data[4])
-            ants[ano] = [ana, E, N, lng, lat]
-    sak = sorted(list(ants.keys()))
-    # with open(fn, 'w') as fp:
-    #     for k in sak:
-    #         x = ants[k]
-    #         s = '{:5s}  {:.2f} {:.2f} {:.4f} {:.4f}\n'.format(x[0], x[1], x[2], x[3], x[4])
-    #         fp.write(s)
+#! /usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+from __future__ import print_function
 
-    num_per_row = 15
-    with open('listants.txt', 'w') as fp:
-        for i in range(0, len(sak), num_per_row):
-            s = ' '.join(['{:>3s}'.format(str(x)) for x in sak[i: i + num_per_row]])
-            print(s)
-            fp.write(s + '\n')
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('filename', help="Filename to sort [installed.txt]", default='installed.txt')
+ap.add_argument('--listed', help="Filename for formatted listed output [listants.txt]", default='listants.txt')
+ap.add_argument('--num_per_row', help="Number to print per row in listed output [15]", default=15)
+args = ap.parse_args()
+
+ants = {}
+with open(args.filename, 'r') as fp:
+    for line in fp:
+        data = line.split()
+        ano = int(data[0][2:])
+        ana = data[0]
+        E = float(data[1])
+        N = float(data[2])
+        lng = float(data[3])
+        lat = float(data[4])
+        ants[ano] = [ana, E, N, lng, lat]
+sak = sorted(list(ants.keys()))
+with open(args.filename, 'w') as fp:
+    for k in sak:
+        x = ants[k]
+        s = '{:5s}  {:.2f} {:.2f} {:.4f} {:.4f}\n'.format(x[0], x[1], x[2], x[3], x[4])
+        fp.write(s)
+
+with open(args.listed, 'w') as fp:
+    for i in range(0, len(sak), args.num_per_row):
+        s = ' '.join(['{:>3s}'.format(str(x)) for x in sak[i: i + args.num_per_row]])
+        print(s)
+        fp.write(s + '\n')
