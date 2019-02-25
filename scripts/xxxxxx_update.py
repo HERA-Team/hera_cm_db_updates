426503dcee14b4e6b99afd876fe775cf7b4a9476
ddeboer <ddeboer@berkeley.edu>
2019-02-25 10:32:45 -0800

    improved xxxxxx sample file

--- scripts/xxxxxx_update.py
+++ scripts/xxxxxx_update.py
@@ -16,27 +16,30 @@ hera.add_part_info(hpn='PAMxxx', rev='A', note="testing", cdate="2019/0", ctime=
 hera.add_full(ant=2, feed=8, fem=17, pam=23, snap='C57', snap_input='e6,n4', cdate='2019/02/20')
 
 # Sample to start/stop parts/connections
-connections_to_stop = []
-parts_to_stop = []
-parts_to_add = []
+connections_to_stop = [
+                      [['CRF001', 'A', 'nb'], ['PAM045', 'A', 'na'], '2019/03/00', '11:00']
+]
+parts_to_stop = [
+                [['PAM028', 'A'], '2019/03/00', '11:30']
+]
+parts_to_add = [
+               [['PAM034', 'A', 'post-amp', '034'], '2019/03/00', '12:00']
+]
 connections_to_add = [
-                     [['CRF001', 'A', 'eb'], ['PAM024', 'A', 'ea'], '2019/02/23', '12:10'],
-                     [['CRF001', 'A', 'nb'], ['PAM024', 'A', 'na'], '2019/02/23', '12:10'],
-                     [['PAM024', 'A', 'eb'], ['SNPC000057', 'A', 'e10'], '2019/02/23', '12:10'],
-                     [['PAM024', 'A', 'nb'], ['SNPC000057', 'A', 'n8'], '2019/02/23', '12:10']
+                     [['CRF001', 'A', 'eb'], ['PAM024', 'A', 'ea'], '2019/02/23', '12:30']
 ]
 
-for c in connections_to_stop:
-    hera.update_connection('stop', c[0], c[1], c[2], c[3])
+for up, down, cdate, ctime in connections_to_stop:
+    hera.update_connection('stop', up, down, cdate, ctime)
 
-for p in parts_to_stop:
-    hera.update_part('stop', p[0], p[1], p[2])
+for part, cdate, ctime in parts_to_stop:
+    hera.update_part('stop', part, cdate, ctime)
 
-for p in parts_to_add:
-    hera.update_part('add', p[0], p[1], p[2])
+for part, cdate, ctime in parts_to_add:
+    hera.update_part('add', part, cdate, ctime)
 
-for c in connections_to_add:
-    hera.update_connection('add', c[0], c[1], c[2], c[3])
+for up, down, cdate, ctime in connections_to_add:
+    hera.update_connection('add', up, down, cdate, ctime)
 
 
 hera.done()
