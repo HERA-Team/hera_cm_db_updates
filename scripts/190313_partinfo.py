2b37b2d6662b2de13997ee0625987638943d3dad
ddeboer <ddeboer@berkeley.edu>
2019-03-13 10:37:06 -0700

    add note for ground straps

---
+++ scripts/190313_partinfo.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+from __future__ import absolute_import, division, print_function
+import signal_chain
+import sys
+
+hera = signal_chain.Update(sys.argv[0], do_it=True)
+
+# Sample to add part information
+hera.add_part_info(hpn='HH0', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH2', rev='A', note="Add braid on coax cable to feed\nAdd ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH11', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH14', rev='A', note="Add braid on coax cable to feed\nAdd ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH23', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH24', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+
+
+hera.done()
