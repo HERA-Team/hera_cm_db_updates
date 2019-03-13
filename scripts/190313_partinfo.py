924dd716ec343a54a1cf5672159bbbd0f821b261
ddeboer <ddeboer@berkeley.edu>
2019-03-13 11:20:36 -0700

    fixed sqlite for partinfo add

--- scripts/190313_partinfo.py
+++ scripts/190313_partinfo.py
@@ -7,9 +7,9 @@ hera = signal_chain.Update(sys.argv[0], do_it=True)
 
 # Sample to add part information
 hera.add_part_info(hpn='HH0', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
-hera.add_part_info(hpn='HH2', rev='A', note="Add braid on coax cable to feed\nAdd ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH2', rev='A', note="Add braid on coax cable to feed. \\nAdd ground strap on feed.", cdate="2019/03/13", ctime="10:00")
 hera.add_part_info(hpn='HH11', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
-hera.add_part_info(hpn='HH14', rev='A', note="Add braid on coax cable to feed\nAdd ground strap on feed.", cdate="2019/03/13", ctime="10:00")
+hera.add_part_info(hpn='HH14', rev='A', note="Add braid on coax cable to feed. \\nAdd ground strap on feed.", cdate="2019/03/13", ctime="10:00")
 hera.add_part_info(hpn='HH23', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
 hera.add_part_info(hpn='HH24', rev='A', note="Add ground strap on feed.", cdate="2019/03/13", ctime="10:00")
 
