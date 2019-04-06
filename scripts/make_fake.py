f11d69b70b0f043e3c18aacc6ad4e1a074048d12
ddeboer <ddeboer@berkeley.edu>
2019-04-06 12:08:07 -0700

    updated signal chain and added fake

---
+++ scripts/make_fake.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2019 the HERA Collaboration
+# Licensed under the 2-clause BSD license.
+
+"""
+Script to make a fake hookup for test purposes.
+"""
+
+from __future__ import absolute_import, division, print_function
+from hera_mc import cm_sysdef, cm_utils
+import signal_chain
+import six
+
+# ###########VALUES HERE##############
+ant = 701
+feed = 708
+fem = 717
+pam = 723
+snap = 'Z38'
+snap_input = 'e10,n8'
+snap_loc = 701
+node = 700
+cdate = '2019/02/20'
+ctime = '10:00'
+# ####################################
+
+part_add_time = int(cm_utils.get_astropytime(cdate, ctime).gps)
+
+fake = signal_chain.Update(log_file=None)
+parts, connections = fake.add_full(ant=ant, feed=feed, fem=fem, pam=pam, snap=snap, snap_input=snap_input,
+                                   snap_loc=snap_loc, node=node, cdate=cdate)
+print("Cut-and-paste into test 'initialization_data_parts.csv'")
+print("-------------------------------------------------------\n")
+for p, x in six.iteritems(parts):
+    print('{},{},{},{},{},'.format(x[0], x[1], x[2], x[3], part_add_time))
+print("\n-------------------------------------------------------\n")
+
+print("Cut-and-paste into test 'initialization_data_connections.csv'")
+print("-------------------------------------------------------------\n")
+for c in connections:
+    conn_add_time = int(cm_utils.get_astropytime(c[2], c[3]).gps)
+    print("{},{},{},{},{},{},{},".format(c[0][0], c[0][1], c[1][0], c[1][1], c[0][2], c[1][2], conn_add_time))
+print("\n-------------------------------------------------------------")
--- scripts/signal_chain.py
+++ scripts/signal_chain.py
@@ -26,7 +26,7 @@ def as_connect(add_or_stop, up, dn, cdate, ctime, do_it):
 
 
 class Update:
-    def __init__(self, exename, do_it, log_file='scripts.log'):
+    def __init__(self, exename=None, do_it=True, log_file='scripts.log'):
         """
         exename:  the name of the script executed (argv[0])
         do_it:  flag to actually enable changes
@@ -34,16 +34,17 @@ class Update:
         """
         self.do_it = do_it
         self.log_file = log_file
-        input_script = os.path.basename(exename)
-        self.output_script = input_script.split('.')[0]
-        print("Writing script {}".format(self.output_script))
-        self.fp = open(self.output_script, 'w')
-        s = '#! /usr/bin/env bash\n'
-        s += 'echo "{} (do_it = {})" >> {} \n'.format(self.output_script, self.do_it, self.log_file)
-        self.fp.write(s)
-        print('-----------------\n')
+        if log_file is not None:
+            input_script = os.path.basename(exename)
+            self.output_script = input_script.split('.')[0]
+            print("Writing script {}".format(self.output_script))
+            self.fp = open(self.output_script, 'w')
+            s = '#! /usr/bin/env bash\n'
+            s += 'echo "{} (do_it = {})" >> {} \n'.format(self.output_script, self.do_it, self.log_file)
+            self.fp.write(s)
+            print('-----------------\n')
 
-    def add_full(self, ant, feed, fem, pam, snap, snap_input, cdate, ctime=['10:00', '11:00'], ser_num={}):
+    def add_full(self, ant, feed, fem, pam, snap, snap_input, snap_loc, node, cdate, ctime=['10:00', '11:00'], ser_num={}):
         """
         Parameters:
         -----------
@@ -53,6 +54,8 @@ class Update:
         pam: pam number (int)
         snap:  snap designation (string: e.g. 'A27')
         snap_input: inputs (string: e.g. 'e10,n8')
+        snap_loc:  location of snap (int)
+        node:  node number (int)
         ser_no:  dictionary of serial numbers (keyed by part-type, uses hpn if not included
         cdate:  YYYY/MM/DD - date of mods (one for all)
         ctime:  time of mods <'10:00', '11:00'>
@@ -71,78 +74,91 @@ class Update:
         self.ser_num_dict = ser_num
 
         # Set up parts
-        part = {}
+        part_to_add = {}
 
         hpn = 'A{}'.format(ant)
         sn = self.get_ser_num(hpn, 'antenna')
-        part['ant'] = (hpn, 'H', 'antenna', sn)
+        part_to_add['ant'] = (hpn, 'H', 'antenna', sn)
 
         hpn = 'FDV{}'.format(feed)
         sn = self.get_ser_num(hpn, 'feed')
-        part['feed'] = (hpn, 'A', 'feed', sn)
+        part_to_add['feed'] = (hpn, 'A', 'feed', sn)
 
         hpn = 'FEM{:03d}'.format(fem)
         sn = self.get_ser_num(hpn, 'front-end')
-        part['fem'] = (hpn, 'A', 'front-end', sn)
+        part_to_add['fem'] = (hpn, 'A', 'front-end', sn)
 
         hpn = 'CRF{:03d}'.format(ant)
         sn = self.get_ser_num(hpn, 'cable-rfof')
-        part['cable'] = (hpn, 'A', 'cable-rfof', sn)
+        part_to_add['cable'] = (hpn, 'A', 'cable-rfof', sn)
 
         hpn = 'PAM{:03d}'.format(pam)
         sn = self.get_ser_num(hpn, 'post-amp')
-        part['pam'] = (hpn, 'A', 'post-amp', sn)
+        part_to_add['pam'] = (hpn, 'A', 'post-amp', sn)
 
         hpn = 'SNP{}{:06d}'.format(snap[0], int(snap[1:]))
         sn = self.get_ser_num(hpn, 'snap')
-        part['snap'] = (hpn, 'A', 'snap', sn)
+        part_to_add['snap'] = (hpn, 'A', 'snap', sn)
         snap_port = {'e': snap_input.split(',')[0].strip(), 'n': snap_input.split(',')[1].strip()}
 
+        hpn = 'N{}'.format(snap_loc)
+        sn = self.get_ser_num(hpn, 'node')
+        part_to_add['node'] = (hpn, 'A', 'node', sn)
+        snap_loc = "loc{}".format(snap_loc)
+
         # Check for parts to add and add them
-        parts_to_add = []
-        for p in six.itervalues(part):
-            x = handle.get_part_dossier(p[0], p[1], 'now')
-            if not len(x):
-                self.update_part('add', p, cdate, partadd_time)
-            else:
-                print("Part {} is already added".format(p))
+        if self.log_file is not None:
+            for p in six.itervalues(part_to_add):
+                x = handle.get_part_dossier(p[0], p[1], 'now')
+                if not len(x):
+                    self.update_part('add', p, cdate, partadd_time)
+                else:
+                    print("Part {} is already added".format(p))
+
         # Set up connections
-        connections_to_add = []
-        up = [part['ant'][0], part['ant'][1], 'focus']
-        dn = [part['feed'][0], part['feed'][1], 'input']
-        connections_to_add.append([up, dn, cdate, connadd_time])
+        connection_to_add = []
+        up = [part_to_add['ant'][0], part_to_add['ant'][1], 'focus']
+        dn = [part_to_add['feed'][0], part_to_add['feed'][1], 'input']
+        connection_to_add.append([up, dn, cdate, connadd_time])
 
-        up = [part['feed'][0], part['feed'][1], 'terminals']
-        dn = [part['fem'][0], part['fem'][1], 'input']
-        connections_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['feed'][0], part_to_add['feed'][1], 'terminals']
+        dn = [part_to_add['fem'][0], part_to_add['fem'][1], 'input']
+        connection_to_add.append([up, dn, cdate, connadd_time])
 
-        up = [part['fem'][0], part['fem'][1], 'e']
-        dn = [part['cable'][0], part['cable'][1], 'ea']
-        connections_to_add.append([up, dn, cdate, connadd_time])
-        up = [part['fem'][0], part['fem'][1], 'n']
-        dn = [part['cable'][0], part['cable'][1], 'na']
-        connections_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['fem'][0], part_to_add['fem'][1], 'e']
+        dn = [part_to_add['cable'][0], part_to_add['cable'][1], 'ea']
+        connection_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['fem'][0], part_to_add['fem'][1], 'n']
+        dn = [part_to_add['cable'][0], part_to_add['cable'][1], 'na']
+        connection_to_add.append([up, dn, cdate, connadd_time])
 
-        up = [part['cable'][0], part['cable'][1], 'eb']
-        dn = [part['pam'][0], part['pam'][1], 'ea']
-        connections_to_add.append([up, dn, cdate, connadd_time])
-        up = [part['cable'][0], part['cable'][1], 'nb']
-        dn = [part['pam'][0], part['pam'][1], 'na']
-        connections_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['cable'][0], part_to_add['cable'][1], 'eb']
+        dn = [part_to_add['pam'][0], part_to_add['pam'][1], 'ea']
+        connection_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['cable'][0], part_to_add['cable'][1], 'nb']
+        dn = [part_to_add['pam'][0], part_to_add['pam'][1], 'na']
+        connection_to_add.append([up, dn, cdate, connadd_time])
 
-        up = [part['pam'][0], part['pam'][1], 'eb']
-        dn = [part['snap'][0], part['snap'][1], snap_port['e']]
-        connections_to_add.append([up, dn, cdate, connadd_time])
-        up = [part['pam'][0], part['pam'][1], 'nb']
-        dn = [part['snap'][0], part['snap'][1], snap_port['n']]
-        connections_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['pam'][0], part_to_add['pam'][1], 'eb']
+        dn = [part_to_add['snap'][0], part_to_add['snap'][1], snap_port['e']]
+        connection_to_add.append([up, dn, cdate, connadd_time])
+        up = [part_to_add['pam'][0], part_to_add['pam'][1], 'nb']
+        dn = [part_to_add['snap'][0], part_to_add['snap'][1], snap_port['n']]
+        connection_to_add.append([up, dn, cdate, connadd_time])
+
+        up = [part_to_add['snap'][0], part_to_add['snap'][1], 'rack']
+        dn = [part_to_add['node'][0], part_to_add['node'][1], snap_loc]
+        connection_to_add.append([up, dn, cdate, connadd_time])
 
         # Check for connections to add and add them
-        for up, down, codate, cotime in connections_to_add:
-            exco = health.check_for_existing_connection(up + down, 'now', display_results=True)
-            if not exco:
-                self.update_connection('add', up, down, codate, cotime)
-        print('\n')
+        if self.log_file is not None:
+            for up, down, codate, cotime in connection_to_add:
+                exco = health.check_for_existing_connection(up + down, 'now', display_results=True)
+                if not exco:
+                    self.update_connection('add', up, down, codate, cotime)
+            print('\n')
+        if self.log_file is None:
+            return part_to_add, connection_to_add
 
     def get_ser_num(self, hpn, part_type):
         sn = hpn
