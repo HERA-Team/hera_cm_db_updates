a3a60b3fd1671fddd3d98d0713848755e3e729f9
David DeBoer <ddeboer@berkeley.edu>
2019-11-21 11:17:15 -0800

    updated add stations

--- cm_updates/191121_add_stations.py
+++ cm_updates/191121_add_stations.py
@@ -10,7 +10,11 @@ print("Updates antennas per googlesheet https://docs.google.com/spreadsheets/d/1
 
 hera.add_antenna_station(stn=249, ser_num=279, cdate='2019/9/6')
 hera.add_antenna_station(stn=232, ser_num=280, cdate='2019/9/6')
-hera.add_antenna_station(stn=214, ser_num=281, cdate='2019/11/6')
-hera.add_antenna_station(stn=174, ser_num=282, cdate='2019/11/7')
+hera.add_antenna_station(stn=213, ser_num=281, cdate='2019/10/22')
+hera.add_antenna_station(stn=154, ser_num=282, cdate='2019/11/4')
+hera.add_antenna_station(stn=175, ser_num=283, cdate='2019/11/5')
+hera.add_antenna_station(stn=214, ser_num=284, cdate='2019/11/6')
+hera.add_antenna_station(stn=174, ser_num=285, cdate='2019/11/7')
+hera.add_antenna_station(stn=194, ser_num=286, cdate='2019/11/7')
 
 hera.done()
