759899cd0cbeaf02b22cb2c5f401f99fa3f4f3bf
David DeBoer <ddeboer@berkeley.edu>
2019-12-03 12:47:35 -0800

    make update info script work

--- scripts/update_info.py
+++ scripts/update_info.py
@@ -4,13 +4,13 @@
 # Licensed under the 2-clause BSD license.
 
 import argparse
-import upd_info
+from hera_cm import upd_info
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('--arc-path', dest='arc_path', help="Path for update archive.", default=None)
     ap.add_argument('--exe-path', dest='exe_path', help="Path for cron script", default='./')
-    ap.add_argument('--verbose', help="Turn verbosity on.", action='store_true')
+    ap.add_argument('-v', '--verbose', help="Turn verbosity on.", action='store_true')
     ap.add_argument('-d', '--duplication_window', help="Number of days to use for duplicate comments.",
                     default=60.0)
     args = ap.parse_args()
