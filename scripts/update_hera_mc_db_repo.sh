846e85b20e439560f8c0dd75e6a2612ee3bf479c
HERA Observer <obs@qmaster>
2020-02-13 18:22:38 +0000

    Change update script

--- scripts/update_hera_mc_db_repo.sh
+++ scripts/update_hera_mc_db_repo.sh
@@ -1,4 +1,10 @@
-#! /usr/bin/env sh
+#!/bin/bash
+
+# set up environment
+source ~/.bashrc
+conda activate HERA
+
+# run scripts to write new hera_cm_db_updates snapshot
 write_sqlite.py
 cm_pack.py --go
 git push origin master
