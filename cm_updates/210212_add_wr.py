d51c686ad7795e78c8b614f624a75b38dadddddd
David DeBoer <ddeboer@berkeley.edu>
2021-02-16 16:34:32 -0800

    update add wr and fix

--- cm_updates/210212_add_wr.py
+++ cm_updates/210212_add_wr.py
@@ -9,15 +9,15 @@ ippre = '10.80.2'
 wrs = [157, 99, 98, 88, 155, 156, 100, 158, 95]
 mfgs = ['S10.486', 'S10.495', 'S10.490', 'S9.393', 'S10.475', 'S10.474', 'S10.483',
         'S10.487', 'S10.488']
-ncms = [23, 24, 25, 26, 27, 18, 19, 30, 31]
+ncms = [23, 24, 25, 26, 27, 28, 29, 30, 31]
 macs = ['6e:b8:f5', '6e:a7:8b', '6e:cd:a4', 'b2:55:30', '6e:6c:bc', '6e:6e:f0',
         '6e:7d:ca', '6e:b8:fa', '6e:b9:75']
-ips = []
+ips = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 
-for _wr, _mf, _nc, _mc, in zip(wrs, mfgs, ncms, macs):
+for _wr, _mf, _nc, _mc, _ip in zip(wrs, mfgs, ncms, macs, ips):
     wrhpn, wrev = hpngen.wr(_wr, 'A')
     part = [wrhpn, wrev, 'white-rabbit', _mf]
-    hera.update_part('add', part, '2020/03/01', '10:00')
+    hera.update_part('add', part, '2021/02/01', '9:00')
     nchpn, nrev = hpngen.ncm(_nc)
     if isinstance(_nc, str) and _nc[0] == 'P':
         oe = 'e'
