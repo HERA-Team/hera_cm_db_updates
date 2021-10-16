7a21b14413b72ba769d8d29eee28233c6370f6d2
Dave DeBoer <ddeboer@berkeley.edu>
2021-10-15 23:54:10 -0700

    add list redis keys

---
+++ hera_cm/list_all_redis_keys.py
@@ -0,0 +1,91 @@
+"""List all redis keys"""
+import redis
+
+
+class RedisKeys():
+
+    def __init__(self):
+        connection_pool = redis.ConnectionPool(host='redishost', decode_responses=True)
+        self.r = redis.StrictRedis(connection_pool=connection_pool, charset='utf-8')
+        self.all_keys = sorted(self.r.keys())
+        self.hashvals = {}
+        self.keyvals = {}
+
+    def _cull_entry(self, key, splitchar=':'):
+        if self.cull is None:
+            return key
+        culled_key = splitchar.join(key.split(splitchar)[:self.cull])
+        self.culled.setdefault(culled_key, [])
+        self.culled[culled_key].append(key)
+        return culled_key
+
+    def split(self, cull=None):
+        self.cull = cull
+        self.culled = {}
+        for ak in self.all_keys:
+            cak = self._cull_entry(ak)
+            try:
+                sahk = sorted(self.r.hkeys(ak))
+                if cak in self.hashvals:
+                    continue
+                for ahk in sahk:
+                    try:
+                        val = self.r.hget(ak, ahk)
+                    except UnicodeDecodeError:
+                        val = '<<byte-data>>'
+                    self.hashvals[cak] = {ahk: val}
+            except redis.ResponseError:
+                if cak in self.keyvals:
+                    continue
+                try:
+                    val = self.r.get(ak)
+                except (redis.ResponseError, UnicodeDecodeError):
+                    val = '<<byte-data>>'
+                self.keyvals[cak] = val
+
+    def write_cull_info(self, fn='key_overview.txt'):
+        print(f"Writing {fn}")
+        single = []
+        with open(fn, 'w') as fp:
+            for ck, cv in self.culled.items():
+                if len(cv) > 1:
+                    print(f"{ck:20s}: {len(cv):03d} - {cv[0]}", file=fp)
+                else:
+                    single.append(ck)
+            print("\n----------Single----------", file=fp)
+            print(f"{', '.join(single)}", file=fp)
+
+    def write_kv(self, fn='key_vals.txt', trunc=35):
+        print(f"Writing {fn}")
+        with open(fn, 'w') as fp:
+            for k, v in self.keyvals.items():
+                if self.cull is None:
+                    cnt = ""
+                else:
+                    cnt = f"{len(self.culled[k]):03d}"
+                print("---------{:20s}  {}  -  {}".format(k, cnt, v[:trunc]), file=fp)
+
+    def write_hv(self, fn='hash_vals.txt', trunc=35):
+        print(f"Writing {fn}")
+        with open(fn, 'w') as fp:
+            for k, v in self.hashvals.items():
+                if self.cull is None:
+                    cnt = ""
+                else:
+                    cnt = f"({len(self.culled[k])})"
+                print("---------{}  {}".format(k, cnt), file=fp)
+                for k2, v2 in v.items():
+                    print("\t{}: {}".format(k2, v2[:trunc]), file=fp)
+#
+# with open('allhkeys.txt', 'w') as fp:
+#     for ah in sorted(list(all_hash.keys())):
+#         print("----------{}".format(ah), file=fp)
+#         for ahk in all_hash[ah]:
+#             print("\t-----{}".format(ahk), file=fp)
+#             try:
+#                 val = r.hget(ah, ahk)
+#             except UnicodeDecodeError:
+#                 val = '<<byte-data>>'
+#             print("\t{}".format(val), file=fp)
+#             #for k, v in val.items():
+#             #    print(f"\t\t{k}:   {v}", file=fp)
