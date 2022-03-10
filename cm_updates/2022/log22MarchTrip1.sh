84f6deea8809aa698427d02c40197438cf059e2f
HERA Observer <obs@qmaster>
2022-03-10 05:48:27 +0000

    add 22 March trip log notes 1

---
+++ cm_updates/2022/log22MarchTrip1.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "log.sh" >> scripts.log 
+add_part_info.py -p HH120 -r A -c 'Placed external noise source on input to SNAP on east pol' --date 2022/03/02 --time 16:00
+add_part_info.py -p HH120 -r A -c 'Removed external noise source on east pol and moved to input to SNAP on north pol' --date 2022/03/03 --time 16:00
+add_part_info.py -p HH120 -r A -c 'External noise source on north pol.' --date 2022/03/04 --time 16:00
+add_part_info.py -p HH120 -r A -c 'External noise source on north pol.' --date 2022/03/05 --time 16:00
+add_part_info.py -p HH120 -r A -c 'Used a splitter on input to snap of north pol to feed antenna to snap and total power meter' --date 2022/03/06 --time 16:00
+add_part_info.py -p HH120 -r A -c 'Used a splitter on input to snap of north pol to feed antenna to snap and total power meter' --date 2022/03/07 --time 16:00
+add_part_info.py -p HH120 -r A -c 'Removed splitter and just input external noise source to input of north pol' --date 2022/03/08 --time 16:00
