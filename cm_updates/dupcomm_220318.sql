1aeebbcdba1e174d17da732697d9066a8fb9b66e
HERA Observer <obs@qmaster>
2022-03-18 17:59:55 +0000

    added dupcomm sql file

---
+++ cm_updates/dupcomm_220318.sql
@@ -0,0 +1,7 @@
+delete from part_info where hpn='HH120' and hpn_rev='A' and posting_gpstime='1330531218';
+delete from part_info where hpn='HH120' and hpn_rev='A' and posting_gpstime='1330704018';
+delete from part_info where hpn='HH73' and hpn_rev='A' and posting_gpstime='1320187338';
+delete from part_info where hpn='HH127' and hpn_rev='A' and posting_gpstime='1320187338';
+delete from part_info where hpn='HH130' and hpn_rev='A' and posting_gpstime='1320187338';
+delete from part_info where hpn='HH16' and hpn_rev='A' and posting_gpstime='1320187338';
+delete from part_info where hpn='HH134' and hpn_rev='A' and posting_gpstime='1325389338';
