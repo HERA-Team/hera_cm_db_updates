be3ab1e051d513b24e8ba9f1be3f981c53f3d85f
HERA Observer <obs@qmaster>
2022-05-07 19:40:09 +0000

    updates

---
+++ cm_updates/2022/fem325nbp16.sql
@@ -0,0 +1,1 @@
+insert into connections (upstream_part, up_part_rev, downstream_part, down_part_rev, upstream_output_port, downstream_input_port, start_gpstime) values ('FEM325', 'A', 'NBP16', 'A', 'n', 'n9', 1332074118);
