3c331bdfbd4caf9d35a838d86b4767652fb8350c
David DeBoer <ddeboer@berkeley.edu>
2022-06-15 12:38:12 -0700

    add fix dup script

---
+++ cm_updates/220615_fix_dup.sql
@@ -0,0 +1,3 @@
+delete from connections where upstream_part='FPS18' and downstream_part='N11';
+delete from connections where upstream_part='NCM18' and downstream_part='N11';
+delete from connections where upstream_part='FPS12' and downstream_part='N14';
