8a2f48e2c7f04accb5e96d23b89cea6823cbaaff
David DeBoer <ddeboer@berkeley.edu>
2022-09-22 13:04:28 -0700

    add pam foam ncm update

---
+++ cm_updates/ncmupdate.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+source ~/.bashrc
+add_part_info.py -p NCM14 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM21 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM06 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM16 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM07 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM11 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM18 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM12 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM20 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM04 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM22 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
+add_part_info.py -p NCM09 -r A -c "Update WR-LEN firmware." -l "infoman" --date 2022/09/21 --time 12:00:00
