e1afe4cdb220dbf4c95a135708624d3774766fc9
David DeBoer <ddeboer@berkeley.edu>
2022-09-29 19:19:00 -0700

    add antcorr script

--- scripts/antcorr.py
+++ scripts/antcorr.py
@@ -18,21 +18,24 @@ class AntCorr:
     def __init__(self):
         self.r = redis.Redis('redishost', decode_responses=True)
         snap_ants_str = self.r.hgetall('corr:snap_ants')
-        self.snap_ants = {}
+        self.snap_corr = {}
         for snaphost, indlist in snap_ants_str.items():
-            self.snap_ants[snaphost] = json.loads(indlist)
+            self.snap_corr[snaphost] = json.loads(indlist)
         map_snap_ant_str = json.loads(self.r.hget('corr:map', 'snap_to_ant'))
         self.map_snap_ant = {}
         for snaphost, antpols in map_snap_ant_str.items():
             self.map_snap_ant[snaphost] = []
             for a in [antpols[i] for i in [0, 2, 4]]:
                 if a is not None:
-                    a = a[2: -2]
+                    a = a[2: -1]
                 self.map_snap_ant[snaphost].append(a)
         self.map_ant_snap = json.loads(self.r.hget('corr:map', 'ant_to_snap'))
 
-    def ant_2_host_corr(self):
-        hostname = self.map_snap_ant[self.antcorr]['e']['host']
+    def ant_2_host_corr(self, antno):
+        hostname = self.map_snap_ant[antno]['e']['host']
+        ants = self.map_snap_ant[hostname]
+        corr = self.snap_corr[hostname]
+        print(hostname, ants, corr)
 #print(snap_ants['heraNode16Snap0'])
 #print(map_snap_ant['heraNode16Snap0'])
 #print(map_ant_snap['151']['e']['host'])
