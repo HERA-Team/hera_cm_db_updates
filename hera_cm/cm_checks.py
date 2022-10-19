14d788fbcb626319be874fa2e6bf52f855142aa0
DAVID DEBOER <ddeboer@DAVIDs-MacBook-Pro-2.local>
2022-10-19 14:21:19 -0700

    snap config yaml generator

--- hera_cm/cm_checks.py
+++ hera_cm/cm_checks.py
@@ -3,9 +3,10 @@
 # Licensed under the 2-clause BSD license.
 
 """Series of database checks."""
-from hera_mc import mc, cm_utils, cm_active, cm_sysutils
+from hera_mc import mc, cm_utils, cm_active, cm_sysutils, cm_hookup
 import redis
 from node_control import he_check
+import yaml
 
 
 def _getkeys(this_dict, these_keys, defv):
@@ -18,7 +19,87 @@ def _getkeys(this_dict, these_keys, defv):
         fndkeys.append(x)
     return fndkeys
 
+def get_snap_connections(verbose=False):
+    snpd = {}
+    snaps = cm_hookup.get_hookup('SNP')
+    for this_snap in snaps:
+        hostname = None
+        ctr = 0
+        for inp in snaps[this_snap].hookup:
+            if len(snaps[this_snap].hookup[inp]):
+                ctr += 1
+                node_num = int(snaps[this_snap].hookup[inp][-1].downstream_part[1:])
+                loc_num = int(snaps[this_snap].hookup[inp][-1].downstream_input_port[3:])
+                hostname = f"heraNode{node_num}Snap{loc_num}"
+        if hostname is not None:
+            if verbose:
+                print(f"{hostname}:  {int(ctr / 2)} antennas ({this_snap})")
+            snpd[hostname] = ctr / 2
+    return snpd
 
+
+def snap_config(old_config_file, new_config_file='snap_config.out', ant_limit=208,
+                use_nodes=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20,22,23],
+                skip_hosts=['heraNode24Snap0'],  # just put one in not used to remind
+                start_block='fengines:', end_block='# Data is sent assuming a total'):
+    snap_conn = get_snap_connections()
+    print(f"Reading old config file {old_config_file}")
+    with open(old_config_file, 'r') as fp:
+        sc = yaml.load(fp, Loader=yaml.Loader)
+    antno = 0
+    total_ants = 0
+    phase_switch_index = 1
+    hostname_order = []
+    snp = cm_hookup.get_hookup('SNP')
+    print("Generating new config.")
+    for node in use_nodes:
+        for sloc in range(4):
+            hostname = f"heraNode{node}Snap{sloc}"
+            if hostname not in snap_conn:
+                print(f"\tSkipping {hostname} since no connections.")
+                continue
+            if hostname in skip_hosts:
+                print(f"\tSkipping {hostname} since marked to skip.")
+                continue
+            if hostname not in sc['fengines']:
+                print(f"\tFYI: {hostname} wasn't enabled in {old_config_file}")
+            this_set = []
+            for ctr in range(3):
+                this_set.append(antno)
+                antno += 1
+            if antno < ant_limit:
+                total_ants += len(this_set)
+                hostname_order.append(hostname)
+                sc['fengines'].setdefault(hostname, {'ants': [], 'phase_switch_index': []})
+                sc['fengines'][hostname]['ants'] = f"[{','.join([str(i) for i in this_set])}]"
+                this_set = []
+                for ctr in range(6):
+                    this_set.append(phase_switch_index)
+                    phase_switch_index += 1
+                    if phase_switch_index > 24:
+                        phase_switch_index = 1
+                sc['fengines'][hostname]['phase_switch_index'] = f"[{','.join([str(i) for i in this_set])}]"
+            else:
+                break
+    print(f"Writing new config file {new_config_file}")
+    with open(old_config_file, 'r') as fpin:
+        with open(new_config_file, 'w') as fpout:
+            in_fengine_block = False
+            for line in fpin:
+                if line.startswith(start_block):
+                    in_fengine_block = True
+                    print('fengines:', file=fpout)
+                    for hostname in hostname_order:
+                        print(f"    {hostname}:", file=fpout)
+                        for fld in ['ants', 'phase_switch_index']:
+                            val = sc['fengines'][hostname][fld].strip("'")
+                            print(f"        {fld}: {val}", file=fpout)
+                elif line.startswith(end_block):
+                    in_fengine_block = False
+                    print('#', file=fpout)
+                if not in_fengine_block:
+                    print(line.strip(), file=fpout)
+    print(f"{len(hostname_order)} snaps and {total_ants} antennas.")
 def _notsame(a, b, **kwargs):
     params = {'ignore_case': True, 'ignore_no_data': 1}
     for key, val in kwargs.items():
