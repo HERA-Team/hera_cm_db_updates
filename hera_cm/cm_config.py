cc23d3867b61737a127bc778f6b722a5b0363466
DAVID DEBOER <ddeboer@berkeley.edu>
2022-10-21 11:25:44 -0700

    208 and 231 configs

--- hera_cm/cm_config.py
+++ hera_cm/cm_config.py
@@ -6,6 +6,33 @@
 from hera_mc import mc, cm_hookup, geo_handling
 import yaml
 import matplotlib.pyplot as plt
+from datetime import datetime
+import copy
+
+
+configs = {'208a_221020':
+           {
+            'compile_limit': 208,
+            'use_nodes': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,19,20],
+            'min_connected': 2,
+            'ignore_outriggers': True,
+            'include_hosts': ['heraNode12Snap1'],
+            'skip_hosts': ['heraNode12Snap0']
+           },
+           '231a_221021':
+           {
+            'compile_limit': 231,
+            'use_nodes': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20],
+            'min_connected': 1,
+            'ignore_outriggers': False,
+            'include_hosts': [],
+            'skip_hosts': []
+           },
+          }
+
+
+def ret_str(this_set):
+    return f"[{','.join([str(i) for i in this_set])}]"
 
 
 def get_snap_connections(verbose=False):
@@ -27,104 +54,132 @@ def get_snap_connections(verbose=False):
                     hostinfo[hostname][ant[:2]].append(ant)
     return hostinfo
 
-# def snap_config(old_config_file, new_config_file='snap_config.out',
-#                 ant_limit=208, use_nodes=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,19,20],
-#                 min_connected=2, ignore_outriggers=True,
-#                 include_hosts=['heraNode12Snap1'], skip_hosts=['heraNode12Snap0'],
-#                 start_block='fengines:', end_block='# Data is sent assuming a total'):
-def snap_config(old_config_file, new_config_file='snap_config.out',
-                ant_limit=232, use_nodes=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20],
-                min_connected=1, ignore_outriggers=False,
-                include_hosts=[], skip_hosts=[],
-                start_block='fengines:', end_block='# Data is sent assuming a total'):
-    snap_conn = get_snap_connections()
 
-    print(f"Reading old config file {old_config_file}")
-    with open(old_config_file, 'r') as fp:
-        sc = yaml.load(fp, Loader=yaml.Loader)
-    antno = 0
-    total_ants = 0
-    phase_switch_index = 1
-    hostname_order = []
-    ant_list = []
+class Configuration:
+    def __init__(self, old_config_file):
+        self.hostinfo = get_snap_connections()
+        self.setup_config_gen(old_config_file)
 
-    print("Generating new config.")
-    key2use = 'HH' if ignore_outriggers else 'all'
-    for node in use_nodes:
-        for sloc in range(4):
-            hostname = f"heraNode{node}Snap{sloc}"
-            if hostname not in snap_conn:
-                print(f"    Skipping {hostname} since no connections.")
-                continue
-            if hostname in skip_hosts:
-                print(f"    Skipping {hostname} since marked to skip.")
-                continue
-            if hostname not in sc['fengines']:
-                print(f"    FYI: {hostname} wasn't enabled in {old_config_file}")
-            antconn = len(snap_conn[hostname][key2use])
-            if antconn >= min_connected or hostname in include_hosts:
-                this_set = []
-                for ctr in range(3):
-                    this_set.append(antno)
-                    antno += 1
-                if antno < ant_limit:
-                    ant_per_host = len(snap_conn[hostname]['all'])
-                    if ant_per_host > 3:
-                        print(f"!!!!!!Warning:  more than 3 antennas:  {snap_conn[hostname]['all']}")
+    def setup_config_gen(self, old_config_file):
+        self.old_config_file = old_config_file
+        print(f"Reading old config file {old_config_file}")
+        with open(old_config_file, 'r') as fp:
+            self.sc = yaml.load(fp, Loader=yaml.Loader)
+
+    def get_hostnames_to_use(self,
+                             compile_limit=232,
+                             use_nodes=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,19,20],
+                             min_connected=1, ignore_outriggers=False,
+                             include_hosts=[], skip_hosts=[]):
+        key2use = 'HH' if ignore_outriggers else 'all'
+        self.compile_limit = compile_limit
+        self.use_nodes = use_nodes
+        self.min_connected = min_connected
+        self.ignore_outriggers = ignore_outriggers
+        self.include_hosts = include_hosts
+        self.skip_hosts = skip_hosts
+
+        print("Getting hosts.")
+        hosts_to_use  = copy.copy(include_hosts)
+        stop_loop = False
+        for node in use_nodes:
+            for sloc in range(4):
+                if len(hosts_to_use) * 3 > compile_limit:
+                    stop_loop = True
+                    break
+                else:
+                    this_hostname = f"heraNode{node}Snap{sloc}"
+                    if this_hostname in hosts_to_use:
                         continue
-                    for ant in snap_conn[hostname]['all']:
-                        ant_list.append(ant.split(':')[0])
-                    total_ants += ant_per_host
-                    hostname_order.append(hostname)
-                    sc['fengines'].setdefault(hostname, {'ants': [], 'phase_switch_index': []})
-                    sc['fengines'][hostname]['ants'] = f"[{','.join([str(i) for i in this_set])}]"
-                    this_set = []
-                    for ctr in range(6):
-                        this_set.append(phase_switch_index)
-                        phase_switch_index += 1
-                        if phase_switch_index > 24:
-                            phase_switch_index = 1
-                    sc['fengines'][hostname]['phase_switch_index'] = f"[{','.join([str(i) for i in this_set])}]"
-            else:
-                print(f"    Skipping {hostname} since {antconn} < {min_connected}")
-            if antno >= ant_limit:
+                    if this_hostname in skip_hosts:
+                        print(f"    Skipping {this_hostname} since marked to skip.")
+                        continue
+                    if this_hostname not in self.hostinfo:
+                        print(f"    Skipping {this_hostname} since no connections.")
+                        continue
+                    antconn = len(self.hostinfo[this_hostname][key2use])
+                    if antconn < min_connected:
+                        print(f"    Skipping {this_hostname} since {antconn} < {min_connected}")
+                        continue
+                    if this_hostname not in self.sc['fengines']:
+                        print(f"    FYI: {this_hostname} wasn't enabled in {self.old_config_file}")
+                    hosts_to_use.append(this_hostname)
+            if stop_loop:
                 break
-        if antno >= ant_limit:
-            break
+        self.hosts_to_use = []
+        self.hosts_not_used = []
+        for node in range(max(use_nodes) + 1):
+            for sloc in range(4):
+                this_hostname = f"heraNode{node}Snap{sloc}"
+                if this_hostname in hosts_to_use:
+                    self.hosts_to_use.append(this_hostname)
+                else:
+                    self.hosts_not_used.append(this_hostname)
 
-    print(f"Writing new config file {new_config_file}")
-    with open(old_config_file, 'r') as fpin:
-        with open(new_config_file, 'w') as fpout:
-            in_fengine_block = False
-            for line in fpin:
-                if line.startswith(start_block):
-                    in_fengine_block = True
-                    print("# ---snap_config parameters---", file=fpout)
-                    print(f"# ant_limit={ant_limit}", file=fpout)
-                    print(f"# use_nodes=[{','.join([str(x) for x in use_nodes])}]", file=fpout)
-                    print(f"# min_connected={min_connected}", file=fpout)
-                    print(f"# ignore_outriggers={str(ignore_outriggers)}", file=fpout)
-                    print(f"# include_hosts=[{','.join(include_hosts)}]", file=fpout)
-                    print(f"# skip_hosts=[{','.join(skip_hosts)}]", file=fpout)
-                    print('fengines:', file=fpout)
-                    for hostname in hostname_order:
-                        print(f"    {hostname}:", file=fpout)
-                        for fld in ['ants', 'phase_switch_index']:
-                            val = sc['fengines'][hostname][fld].strip("'")
-                            print(f"        {fld}: {val}", file=fpout)
-                elif line.startswith(end_block):
-                    in_fengine_block = False
-                    print('#', file=fpout)
-                if not in_fengine_block:
-                    print(line.strip(), file=fpout)
-    print(f"{len(hostname_order)} snaps and {total_ants} antennas.")
-    print(f"Highest index {len(hostname_order) * 3}")
-    with mc.MCSessionWrapper() as session:
-        geo = geo_handling.Handling(session)
-        loc = geo.get_location(ant_list, 'now')
-        geo.set_graph(True)
-        geo.plot_all_stations() 
-        geo.plot_stations(loc, xgraph='E', ygraph='N', label='none',
-                          marker_color='k', marker_shape='o', marker_size=4)
-        plt.axis(xmin=540400, xmax=541400)
-    return ant_list
+    def generate_new_fengines(self):
+        self.total_ants = 0
+        self.ant_list = []
+
+        antno = 0
+        phase_switch_index = 1
+        print("Generating new fengines.")
+        self.fengines = {}
+        for this_host in self.hosts_to_use:
+            ant_per_host = len(self.hostinfo[this_host]['all'])
+            if ant_per_host > 3:
+                raise ValueError(f"!!!!!!Warning:  more than 3 antennas:  {self.hostinfo[this_host]['all']}")
+            for ant in self.hostinfo[this_host]['all']:
+                self.ant_list.append(ant.split(':')[0])
+            self.total_ants += ant_per_host
+
+            ant_set = []
+            for ctr in range(3):
+                ant_set.append(antno)
+                antno += 1
+            phase_set = []
+            for ctr in range(6):
+                phase_set.append(phase_switch_index)
+                phase_switch_index += 1
+                if phase_switch_index > 24:
+                    phase_switch_index = 1
+            self.max_antno = antno
+            self.fengines[this_host] = {'ants': ret_str(ant_set), 'phase_switch_index': ret_str(phase_set)}
+   
+    def write_yaml(self, start_block='fengines:', end_block='# Data is sent assuming a total'):
+        wtime = datetime.strftime(datetime.now(), '%y%m%d%H%M')
+        self.new_config_file = f"{self.old_config_file.split('.')[0]}_{self.compile_limit}_{wtime}.yaml"
+        print(f"Writing new config file {self.new_config_file}")
+        with open(self.old_config_file, 'r') as fpin:
+            with open(self.new_config_file, 'w') as fpout:
+                in_fengine_block = False
+                for line in fpin:
+                    if line.startswith(start_block):
+                        in_fengine_block = True
+                        print("# ---snap_config parameters---", file=fpout)
+                        print(f"# compile_limit={self.compile_limit}", file=fpout)
+                        print(f"# use_nodes=[{','.join([str(x) for x in self.use_nodes])}]", file=fpout)
+                        print(f"# min_connected={self.min_connected}", file=fpout)
+                        print(f"# ignore_outriggers={str(self.ignore_outriggers)}", file=fpout)
+                        print(f"# include_hosts=[{','.join(self.include_hosts)}]", file=fpout)
+                        print(f"# skip_hosts=[{','.join(self.skip_hosts)}]", file=fpout)
+                        print('fengines:', file=fpout)
+                        for hostname in self.hosts_to_use:
+                            print(f"    {hostname}:", file=fpout)
+                            for fld in ['ants', 'phase_switch_index']:
+                                val = self.fengines[hostname][fld].strip("'")
+                                print(f"        {fld}: {val}", file=fpout)
+                    elif line.startswith(end_block):
+                        in_fengine_block = False
+                        print('#', file=fpout)
+                    if not in_fengine_block:
+                        print(line.strip(), file=fpout)
+        print(f"{len(self.hosts_to_use)} snaps and {self.total_ants} antennas.")
+        print(f"Highest index {self.max_antno}")
+        with mc.MCSessionWrapper() as session:
+            geo = geo_handling.Handling(session)
+            loc = geo.get_location(self.ant_list, 'now')
+            geo.set_graph(True)
+            geo.plot_all_stations() 
+            geo.plot_stations(loc, xgraph='E', ygraph='N', label='none',
+                            marker_color='k', marker_shape='o', marker_size=4)
+            plt.axis(xmin=540400, xmax=541400)
