6a9dcf320a5eddcf0a51d066ef63decf3b8a45c2
DAVID DEBOER <ddeboer@berkeley.edu>
2022-11-29 10:32:23 -0800

    add path to gsheet archive search

--- hera_cm/cm_gsheet.py
+++ hera_cm/cm_gsheet.py
@@ -277,12 +277,12 @@ class ArchiveGsheet:
         import pandas
         from hera_mc.mc import get_cm_csv_path
         if base_path is None:
-            self.base_path = ospath(get_cm_csv_path(), 'cm_updates/gsheet')
+            self.base_path = ospath.join(get_cm_csv_path(), 'cm_updates/gsheet')
         else:
             self.base_path = base_path
         self.node = {}
         for node in range(24):
-            self.node[node] = pandas.read_csv(ospath(base_path, f"node{node}.csv"))
+            self.node[node] = pandas.read_csv(ospath.join(base_path, f"node{node}.csv"))
 
     def find(self, **kwargs):
         from tabulate import tabulate
