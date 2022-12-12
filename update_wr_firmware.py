cf5bd00d14753e98e3fad7a2b75fa7da450669e2
DAVID DEBOER <ddeboer@berkeley.edu>
2022-12-12 05:01:42 -0800

    add wr update

---
+++ update_wr_firmware.py
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "add_ncm" >> scripts.log 
+add_part_info.py -p NCMP2 -r A -c "Update WR firmware" --date 2022/12/05 --time 12:00
+add_part_info.py -p NCM3 -r A -c "Update WR firmware" --date 2022/12/05 --time 12:00
+add_part_info.py -p NCM2 -r A -c "Update WR firmware" --date 2022/12/06 --time 12:00
+add_part_info.py -p NCM10 -r A -c "Update WR firmware" --date 2022/12/06 --time 12:00
+add_part_info.py -p NCM15 -r A -c "Update WR firmware" --date 2022/12/07 --time 12:00
+add_part_info.py -p WRA000028 -r A -c "Update WR firmware" --date 2022/12/05 --time 12:00
+add_part_info.py -p WRA000084 -r A -c "Update WR firmware" --date 2022/12/05 --time 12:00
+add_part_info.py -p WRA000083 -r A -c "Update WR firmware" --date 2022/12/06 --time 12:00
+add_part_info.py -p WRA000097 -r A -c "Update WR firmware" --date 2022/12/06 --time 12:00
+add_part_info.py -p WRA000085 -r A -c "Update WR firmware" --date 2022/12/07 --time 12:00
