2ced807f6eaa2c767d87d005685980d5d845ddde
HERA Observer <obs@qmaster>
2023-03-29 20:45:12 +0000

    working collapse connupd

--- hera_cm/collapse_connupd.py
+++ hera_cm/collapse_connupd.py
@@ -44,47 +44,17 @@ class CollapseConn:
                         vtime = getattr(arg, 'time')
                         datekey = datetime.datetime.strptime(f"{vdate}T{vtime}", "%Y/%m/%dT%H:%M")
                         self.cmds[cmd][key][datekey] =  line.strip()
-    def get_cmds(self, cmds=['add_part', 'stop_connection', 'add_connection']):
+
+    def write_cc_file(self, cmds=['add_part', 'stop_connection', 'add_connection']):
         now = datetime.datetime.now()
-        fn = f"{now.strftime('%Y%m%d')}_connupd_{now.strftime('%H%M')}"
-        print(fn)
-# fprm = open('rm_connupd', 'w')
-
-# connupd_data = []
-# connupd_track = []
-# for ifile in connupd_files:
-#     path_ifile = os.path.join(args.directory, ifile)
-#     fprm.write("rm -f {}\n".format(path_ifile))
-#     dstr = ifile.split('_')[0]
-#     try:
-#         yr = int(dstr[:2])
-#     except ValueError:
-#         continue
-#     mn = int(dstr[2:4])
-#     dy = int(dstr[4:6])
-#     dt = datetime.datetime(year=yr, month=mn, day=dy)
-#     if dt < args.search_after:
-#         continue
-#     with open(path_ifile, 'r') as fp:
-#         for line in fp:
-#             found_keyword = False
-#             for ky in args.keywords:
-#                 if line.startswith(ky):
-#                     found_keyword = True
-#                     if ky not in connupd_track:
-#                         connupd_data.append(line.strip())
-#                         connupd_track.append(ky)
-#                     break
-#             if found_keyword:
-#                 continue
-#             if '--date' in line:
-#                 data = line[:line.index('--date')]
-#             else:
-#                 data = line[:].strip()
-#             if data not in connupd_track:
-#                 connupd_data.append(line.strip())
-#                 connupd_track.append(data)
-
-# with open('cat_connupd.txt', 'w') as fp:
-#     for x in connupd_data:
-#         print(x, file=fp)
+        fn = f"cc{now.strftime('%y%m%d')}_connupd_{now.strftime('%H%M')}"
+        with open(fn, 'w') as fp:
+            print(f"Writing {fn}")
+            print("#! /bin/bash", file=fp)
+            print("source ~/.bashrc", file=fp)
+            print(f"echo {fn} >> scripts.log", file=fp)
+            for cmd in cmds:
+                print(f"# {cmd}", file=fp)
+                for entry in self.cmds[cmd]:
+                    early = min(self.cmds[cmd][entry])
+                    print(f"{cmd}.py {self.cmds[cmd][entry][early]}", file=fp)
