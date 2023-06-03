fff5bb3fb995bbb5bcc8d93098010a71c04f8e41
DAVID DEBOER <ddeboer@berkeley.edu>
2023-06-02 23:03:00 -0700

    add npb 14 db update

---
+++ cm_updates/2023/nbp14.sql
@@ -0,0 +1,3 @@
+update connections set stop_gpstime=1268391610 where upstream_part='FEM198' and start_gpstime=1268391858;
+update connections set stop_gpstime=1268391610 where upstream_part='FEM198' and start_gpstime=1339713618;
+update connections set stop_gpstime=NULL where upstream_part='FEM369' and start_gpstime=1353778518;
