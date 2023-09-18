5a63e4ba635444b2a8f65fba3bd950890725b84a
DAVID DEBOER <ddeboer@berkeley.edu>
2023-09-18 06:34:12 -0700

    cm_notes.py in final form

--- scripts/cm_notes.py
+++ scripts/cm_notes.py
@@ -3,18 +3,40 @@
 # Copyright 2023 the HERA Collaboration
 # Licensed under the 2-clause BSD license.
 
-"""Script to get notes within a given day."""
+"""Script to get notes by date."""
 
 import argparse
 from hera_mc import cm_active, cm_utils
 from tabulate import tabulate
+import datetime
+
+dhelpstr = "Number of days (+/-) around date to look.\n"\
+           "If >= 0, the days argument will just search on calendar days over that many days, e.g. 2023/09/15 ignoring time.\n"\
+           "If < 0, the days argument will search for comments within that many (fractional) days from posting time."
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-d', '--days', help="Number of days (+/-) around date to look.", type=float, default=1.0)
+ap.add_argument('-d', '--days', help=dhelpstr, type=float, default=1.0)
+ap.add_argument('-s', '--sortby', help="Sort output by 'part' or 'date' first", choices=['part', 'date'], default='date')
 cm_utils.add_date_time_args(ap)
 args = ap.parse_args()
 
-date = cm_utils.get_astropytime(args.date)
+date = cm_utils.get_astropytime(args.date, args.format)
+
+if args.days < 0.0:
+    args.day = abs(args.days)
+    caldate = None
+else:
+    args.days = int(args.days)
+    caldate = []
+    for i in range(-args.days, args.days+1):
+        caldate.append((date.datetime + datetime.timedelta(days=i)).strftime('%Y%m%d'))
+
+def date_check(posting):
+    if caldate is None:
+        return abs(posting - date.gps) < (args.days * 3600 * 24)
+    if cm_utils.get_astropytime(posting, float_format='gps').datetime.strftime('%Y%m%d') in caldate:
+        return True
+    return False
 
 active = cm_active.get_active(date, loading=['info'])
 
@@ -22,10 +44,14 @@ found = {}
 for hpn, notes in active.info.items():
     for note in notes:
         diff = abs(note.posting_gpstime - date.gps)
-        print(diff / (3600*24))
-        if diff < args.days * 3600 * 24:
+        if date_check(note.posting_gpstime):
             note.date = cm_utils.get_astropytime(note.posting_gpstime, float_format='gps')
-            found[note.date.datetime] = note
+            nk, pk, rk = cm_utils.peel_key(note.hpn, 'NPR')
+            if args.sortby == 'date':
+                key = f"{note.date.datetime}{pk}{nk:04d}{note.comment}"
+            else:
+                key = f"{pk}{nk:04d}{note.date.datetime}{note.comment}"
+            found[key] = note
 
 table_header = ['Date', 'HPN', 'Note']
 table_data = []
